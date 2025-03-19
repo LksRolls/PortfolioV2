2c8b8453b971acfd277460feb8aac33b81917c7d
LukasRolls <125378101+LksRolls@users.noreply.github.com>
2025-03-19 16:29:05 +0100

    Add files via upload
    Base du portfolio et header

---
+++ assets/scripts/script.js
@@ -0,0 +1,14 @@
+// Menu burger
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    document.getElementById('mobile-menu').classList.toggle('hidden');
+});
+
+// Menu déroulant mobile Présentation
+document.getElementById('mobile-presentation').addEventListener('click', function () {
+    document.getElementById('mobile-presentation-menu').classList.toggle('hidden');
+});
+
+// Menu déroulant mobile Projets
+document.getElementById('mobile-projets').addEventListener('click', function () {
+    document.getElementById('mobile-projets-menu').classList.toggle('hidden');
+});
